257c50d364f8e75ec1f8fb66e0527f2e1a5265cc
totto <totto@totto.org>
2017-12-27 14:50:41 +0100

    Added initial LoadTestConfig

---
+++ src/test/java/no/cantara/service/setup/SetupLoadTestResourceTest.java
@@ -0,0 +1,38 @@
+package no.cantara.service.setup;
+
+import no.cantara.service.testsupport.TestServer;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import static com.jayway.restassured.RestAssured.given;
+
+public class SetupLoadTestResourceTest {
+    private TestServer testServer;
+
+    @BeforeClass
+    public void startServer() throws Exception {
+        testServer = new TestServer(getClass());
+        testServer.start();
+    }
+
+    @AfterClass
+    public void stop() {
+        testServer.stop();
+    }
+
+    @Test
+    public void testHealth() throws IOException {
+        given()
+                .log().everything()
+                .expect()
+                .statusCode(HttpURLConnection.HTTP_OK)
+                .log().everything()
+                .when()
+                .get(SetupLoadTestResource.CONFIG_PATH);
+    }
+
+}
