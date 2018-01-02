d9efdddbfeb68a9580ba29e7ebad57e51830252c
totto <totto@totto.org>
2018-01-02 12:35:26 +0100

    now using hystrix commands in test driver

--- src/test/java/no/cantara/commands/CommandGetOauth2ProtectedPingFallbackTest.java
+++ src/test/java/no/cantara/commands/CommandGetOauth2ProtectedPingFallbackTest.java
@@ -2,6 +2,7 @@ package no.cantara.commands;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -13,6 +14,7 @@ public class CommandGetOauth2ProtectedPingFallbackTest {
     private static String pingURL = "http://195.204.7.28";
 
     @Test
+    @Ignore
     public void testCommandGetOauth2ProtectdPing() throws Exception {
 
 
