e1965cf7e6e2d1db7cb327a3b44894da06a29a30
totto <totto@totto.org>
2018-01-10 23:32:57 +0100

    minor cleanup

--- scripts/run_from_dockerhub.sh
+++ scripts/run_from_dockerhub.sh
@@ -2,7 +2,6 @@
 # This file will build a Docker image, create a data volume container and run an application container
 # from the image. Use it to test that your Docker configuration builds successfully.
 
-# Change these
 PROJECT_NAME=HTTPLoadtest
 DOCKERHUB_ORG=cantara
 IMAGE_NAME=httploadtest-baseline
@@ -10,12 +9,14 @@ IMAGE_NAME_DATA=cantara-httploadtestbaseline-data
 
 sudo docker rm -v -f $IMAGE_NAME
 sudo docker pull $DOCKERHUB_ORG/$IMAGE_NAME
-
 sudo docker create --name $IMAGE_NAME_DATA $DOCKERHUB_ORG/$IMAGE_NAME
-
 sudo docker run -d -p 18086:8086 --name $IMAGE_NAME --volumes-from $IMAGE_NAME_DATA $DOCKERHUB_ORG/$IMAGE_NAME
 
-
-echo "Starting instance. Do 'sudo docker exec -it $DOCKERHUB_ORG/$IMAGE_NAME bash' to get shell"
-
 wget http://localhost:18086/HTTPLoadTest-baseline/health
+cat health
+echo ""
+echo ""
+echo "Starting instance. Do 'sudo docker exec -it $DOCKERHUB_ORG/$IMAGE_NAME bash' to get shell"
+echo ""
+echo "http://localhost:18086/HTTPLoadTest-baseline/config"
+
