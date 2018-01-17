c5b79ea3ceb2937799f5fc64f0ce2f28a684df6c
Kim Christian Gaaarder <kim.christian.gaarder@gmail.com>
2018-01-17 15:42:16 +0100

    Remove unused duplicated functionality.

--- src/main/java/no/cantara/service/loadtest/SingleLoadTestExecution.java
+++ src/main/java/no/cantara/service/loadtest/SingleLoadTestExecution.java
@@ -1,7 +1,6 @@
 package no.cantara.service.loadtest;
 
 import no.cantara.service.Main;
-import no.cantara.service.health.HealthResource;
 import no.cantara.service.loadtest.drivers.LoadTestExecutionContext;
 import no.cantara.service.loadtest.drivers.MyReadRunnable;
 import no.cantara.service.loadtest.drivers.MyRunnable;
@@ -13,11 +12,6 @@ import no.cantara.service.model.TestSpecification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
@@ -213,117 +207,6 @@ public class SingleLoadTestExecution implements LoadTestExecutionContext {
     @Override
     public void addResult(LoadTestResult loadTestResult) {
         LoadTestExecutorService.addResult(loadTestResult); // TODO Circular dependency, this should be avoided!
-    }
-
-    String getPrintableStats(List<LoadTestResult> loadTestResults, boolean whileRunning) {
-        long stopTime;
-        long startTime;
-        int tasksScheduled;
-
-        // synchronize on this instance when reading mutable variables.
-        synchronized (this) {
-            stopTime = this.stopTime;
-            startTime = this.startTime;
-            tasksScheduled = this.tasksScheduled;
-        }
-
-        long nowTimestamp = System.currentTimeMillis();
-        boolean shouldStop;
-
-        shouldStop = stopTime == 0 && ((nowTimestamp - startTime) / 1000) > loadTestConfig.getTest_duration_in_seconds();
-        if (shouldStop) {
-            stop();  // We might get in trouble if no memory for native threads in high thread situations
-        }
-        if (loadTestResults == null || (whileRunning == false && isRunning())) {
-            return "";  // We ship on empty results and if tests are running and whileRunning flag is not set
-        }
-        int r_deviations = 0;
-        int r_success = 0;
-        long r_duration = 0;
-        long w_duration = 0;
-        int r_results = 0;
-        int w_deviations = 0;
-        int w_success = 0;
-        int w_results = 0;
-        int deviations = 0;
-        int success = 0;
-        int results = 0;
-        long r_mean_success = 0;
-        long r_ninety_percentine_success = 0;
-        long w_mean_success = 0;
-        long w_ninety_percentine_success = 0;
-        List<Long> r_times = new ArrayList<>();
-        List<Long> w_times = new ArrayList<>();
-        for (LoadTestResult loadTestResult : loadTestResults) {
-            if (loadTestResult.getTest_id().startsWith("r-")) {
-                r_results++;
-                if (loadTestResult.isTest_deviation_flag()) {
-                    r_deviations++;
-                }
-                if (loadTestResult.isTest_success()) {
-                    r_success++;
-                    r_duration = r_duration + loadTestResult.getTest_duration();
-                    r_times.add(loadTestResult.getTest_duration());
-                }
-
-            } else {
-                if (loadTestResult.getTest_id().startsWith("w-")) {
-                    w_results++;
-                    if (loadTestResult.isTest_deviation_flag()) {
-                        w_deviations++;
-                    }
-                    if (loadTestResult.isTest_success()) {
-                        w_success++;
-                        w_duration = w_duration + loadTestResult.getTest_duration();
-                        w_times.add(loadTestResult.getTest_duration());
-                    }
-
-                } else {
-                    results++;
-                    if (loadTestResult.isTest_deviation_flag()) {
-                        deviations++;
-                    }
-                    if (loadTestResult.isTest_success()) {
-                        success++;
-                    }
-
-                }
-            }
-        }
-        String stats = "";
-        DateFormat df = new SimpleDateFormat("dd/MM-yyyy  HH:mm:ss");
-        if (stopTime == 0) {
-            stats = "Started: " + df.format(new Date(startTime)) + " Version:" + HealthResource.getVersion() + "  Now: " + df.format(new Date(nowTimestamp)) + "  Running for " + (nowTimestamp - startTime) / 1000 + " seconds.\n";
-        } else {
-            stats = "Started: " + df.format(new Date(startTime)) + " Version:" + HealthResource.getVersion() + "  Now: " + df.format(new Date(nowTimestamp)) + "  Ran for " + (stopTime - startTime) / 1000 + " seconds.\n";
-        }
-        if (r_success > 0) {
-            r_mean_success = r_duration / r_success;
-            Collections.sort(r_times);
-            r_ninety_percentine_success = r_times.get(r_times.size() * 9 / 10);
-        }
-        if (w_success > 0) {
-            w_mean_success = w_duration / w_success;
-            Collections.sort(w_times);
-            w_ninety_percentine_success = w_times.get(w_times.size() * 9 / 10);
-        }
-        stats = stats + "\n" + String.format(" %4d read tests resulted in %d successful runs where %d was marked failure and %d was marked as deviation(s).", r_results, r_success, (r_results - r_success), r_deviations);
-        stats = stats + "\n" + String.format(" %4d write tests resulted in %d successful runs where %d was marked failure and %d was marked as deviation(s).", w_results, w_success, (w_results - w_success), w_deviations);
-        stats = stats + "\n" + String.format(" %4d unmarked tests resulted in %d successful runs where %d was marked failure and  %d was marked as deviation(s).", results, success, (results - success), deviations);
-        stats = stats + "\n" + String.format(" %4d total tests resulted in %d successful runs where %d was marked failure and %d was marked as deviation(s).", r_results + w_results + results, r_success + w_success + success, (r_results + w_results + results) - (r_success + w_success + success), r_deviations + w_deviations + deviations);
-        stats = stats + "\n" + String.format(" %4d tasks scheduled, number of threads configured: %d,  isRunning: %b ", tasksScheduled, threadPoolSize, isRunning());
-        stats = stats + "\n" + String.format(" %4d ms mean duration for successful read tests, %4d ms ninety percentile successful read tests ", r_mean_success, r_ninety_percentine_success);
-        stats = stats + "\n" + String.format(" %4d ms mean duration for successful write tests, %4d ms ninety percentile successful write tests ", w_mean_success, w_ninety_percentine_success);
-
-        String loadTestJson = "";
-        try {
-            loadTestJson = LoadTestResultUtil.mapper().writerWithDefaultPrettyPrinter().writeValueAsString(loadTestConfig);
-            loadTestJson = loadTestJson + "\n\n";
-
-        } catch (Exception e) {
-            log.warn("Unable to serialize loadTestConfig to json", e);
-        }
-        return stats + "\n\n" + loadTestJson;
     }
 
     public synchronized long getStartTime() {
