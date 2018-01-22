abd6d932bbb8f731fa39ffad86347d52b55e8cce
totto <totto@totto.org>
2018-01-22 13:07:49 +0100

    begun on scripted demo for configuring, running and evaluation result flows for CD pipelines

--- scripts/HTTPLoadTest-QAFlow-Demo/configure_and_run_loadtest.sh
+++ scripts/HTTPLoadTest-QAFlow-Demo/configure_and_run_loadtest.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-wget -v -d "jsonConfig=@ReadTestSpecification-TestHealth.json" -H "Content-Type: application/x-www-form-urlencoded" --user admin --password admin  -post-data http://localhost:8086/HTTPLoadTest-baseline/loadTest/form/read
-wget -v -d "jsonConfig=@WriteTestSpecification_TestOauth2ProtectedResource.json" -H "Content-Type: application/x-www-form-urlencoded" --user admin --password admin   -post-datahttp://localhost:8086/HTTPLoadTest-baseline/loadTest/form/write
-wget -v -d "jsonConfig=@@LoadTestConfig.json" -H "Content-Type: application/x-www-form-urlencoded" --user admin --password admin  -post-data http://localhost:8086/HTTPLoadTest-baseline/loadTest/form
+JSON=$(<ReadTestSpecification-TestHealth.json ); wget -v --post-data "jsonConfig=${JSON}" -X POST  http://localhost:8086/HTTPLoadTest-baseline/loadTest/form/read
+JSON=$(<WriteTestSpecification_TestOauth2ProtectedResource.json ); wget -v --post-data "jsonConfig=${JSON}" -X POST  http://localhost:8086/HTTPLoadTest-baseline/loadTest/form/write
+JSON=$(<LoadTestBenchmark.json ); wget -v --post-data "jsonConfig=${JSON}" -X POST  http://localhost:8086/HTTPLoadTest-baseline/loadTest/form/benchmark
+JSON=$(<LoadTestConfig.json ); wget -v --post-data "jsonConfig=${JSON}" -X POST  http://localhost:8086/HTTPLoadTest-baseline/loadTest/form
+
 
 ##  wait until test is complete
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8086/HTTPLoadTest-baseline/loadTest/runstatus)" == "409" ]]; do sleep 5; done
