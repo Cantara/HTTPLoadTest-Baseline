c42974d5d9aaf63f61ee23be438dd042d76c1cdf
totto <totto@totto.org>
2018-01-31 14:51:34 +0100

    minor

--- src/test/java/no/cantara/service/loadtest/util/FileFinderTest.java
+++ src/test/java/no/cantara/service/loadtest/util/FileFinderTest.java
@@ -14,19 +14,21 @@ public class FileFinderTest {
 
     private static final Logger log = LoggerFactory.getLogger(HTTPResultUtil.class);
     private static final ObjectMapper mapper = new ObjectMapper();
-    public static final String RESULT_FILE_PATH = "./src";
+    public static final String SEARCH_FILE_PATH = "./src/";
 
     @Test
     public void testUsage() throws Exception {
 
 
-        Path startingDir = Paths.get(RESULT_FILE_PATH);
-        String pattern = "*.json";
+        Path startingDir = Paths.get(SEARCH_FILE_PATH);
+        String pattern = "*Spec*.json*";
 
         FileFinder finder = new FileFinder(pattern);
         Files.walkFileTree(startingDir, finder);
         ArrayList<String> filenames = finder.done();
         System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(filenames));
+
+        System.out.println("files found:" + filenames.size());
 
     }
 
